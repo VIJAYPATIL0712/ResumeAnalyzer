b1da86d79c2a18343a227797cc80144a5d76b37b
Vijay Patil <vijay4patil12@gmail.com>
2025-09-16 04:57:38 +0530

    Analysis Controller Edited

--- src/main/java/com/resumeanalyzer/controller/AnalysisController.java
+++ src/main/java/com/resumeanalyzer/controller/AnalysisController.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/analysis")
+@RequestMapping("/api/analysis/abc")
 public class AnalysisController {
 
     @Autowired
